77997fe3e32882de4fd1eb5acdcc5cb4332fa99a
tytan652 <tytan652@tytanium.xyz>
2021-10-05 14:21:01 +0200

    win-dshow: Switch virtual camera flag
    Also change "virtualcam_output" to "dshow_output"

--- plugins/win-dshow/virtualcam.c
+++ plugins/win-dshow/virtualcam.c
@@ -114,8 +114,8 @@ static void virtual_video(void *param, struct video_data *frame)
 }
 
 struct obs_output_info virtualcam_info = {
-	.id = "virtualcam_output",
-	.flags = OBS_OUTPUT_VIDEO,
+	.id = "dshow_output",
+	.flags = OBS_OUTPUT_VIDEO | OBS_OUTPUT_VIRTUALCAM,
 	.get_name = virtualcam_name,
 	.create = virtualcam_create,
 	.destroy = virtualcam_destroy,
